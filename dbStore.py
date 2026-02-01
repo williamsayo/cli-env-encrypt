73eed956a5246121482976a76c03592947de8636
williams ayo <williamsayo44@gmail.com>
2026-02-01 10:21:13 +0200

    Add SQLite manager class for storing and managing file salts

---
+++ dbStore.py
@@ -0,0 +1,54 @@
+from uuid import UUID
+import sqlite3
+from typing import Tuple
+
+
+class DB:
+    SCHEMA = """CREATE TABLE IF NOT EXISTS File_Salts (
+    file_key TEXT PRIMARY KEY,
+    salt BLOB NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
+
+    def __init__(self, db_name: str = "salts.db") -> None:
+        self._init_db(db_name)
+
+    def _init_db(self, db_path: str) -> None:
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = self.connection.cursor()
+
+        with self.connection:
+            self.cursor.execute(self.SCHEMA)
+
+    def save_salt(self, key: UUID, salt: bytes) -> None:
+        try:
+           with self.connection:
+                self.cursor.execute(
+                    "INSERT OR REPLACE INTO File_Salts (file_key, salt) VALUES (:key, :salt);",
+                    {"key": str(key), "salt": salt},
+                )
+        except Exception as e:
+            print(e) 
+
+    def delete_salt(self, key: UUID) -> None:
+        with self.connection:
+            self.cursor.execute(
+                "DELETE FROM File_Salts WHERE file_key = :key;",
+                {"key": str(key)},
+            )
+
+    def get_salt(self, key: UUID) -> Tuple[bytes, str]:
+        with self.connection:
+            self.cursor.execute(
+                "SELECT salt,created_at FROM File_Salts WHERE file_key = :key;",
+                {"key": str(key)},
+            )
+            result = self.cursor.fetchone()
+            if result:
+                return result
+            else:
+                raise KeyError(f"No salt found for key: {key}")
+
+    def close(self) -> None:
+        self.connection.close()
